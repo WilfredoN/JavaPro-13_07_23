0b2a7e89a435992e72e06f388a2a231f0f0bc729
Nikita <59343780+WilfredoN@users.noreply.github.com>
2023-10-01 20:13:32 +0300

    ДЗ 23. Операції TCL

--- run-db.bat
+++ run-db.bat
@@ -2,5 +2,5 @@ docker run -d ^
   -e POSTGRES_USER=hillel ^
   -e POSTGRES_PASSWORD=hillel ^
   -p 5432:5432 ^
-  -v java-pro-202307:/var/lib/postgresql/data ^
+  -v java-pro:/var/lib/postgresql/data ^
   postgres:14.2
