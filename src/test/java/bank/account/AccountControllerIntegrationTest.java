cbac61d0b306984276fade313389fb12908ce892
Nikita <thekillermaps@gmail.com>
2023-11-28 22:47:30 +0200

    fixes

--- src/test/java/bank/account/AccountControllerIntegrationTest.java
+++ src/test/java/bank/account/AccountControllerIntegrationTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class AccountControllerIntegrationTest {
-    public String uid = UUID.randomUUID().toString();
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -30,18 +30,20 @@ class AccountControllerIntegrationTest {
 
     @Test
     public void shouldCreateAccount() throws Exception {
+        String personUid = UUID.randomUUID().toString();
+        String accountUid = UUID.randomUUID().toString();
         var person = personRepository.save(Person.builder()
-                .uid(uid)
+                .uid(personUid)
                 .name("Name Test")
                 .email("testing@test.com")
                 .build());
         var account = accountRepository.save(Account.builder()
                 .iban("UA1234567890123456789012")
-                .uid(UUID.randomUUID().toString())
+                .uid(accountUid)
                 .balance(512)
                 .person(person)
                 .build());
-        mockMvc.perform(get("/api/person/" + uid + "/accounts"))
+        mockMvc.perform(get("/api/person/" + personUid + "/accounts"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
