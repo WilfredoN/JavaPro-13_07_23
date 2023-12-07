920f2b078039920393169295d6b949968b544a0f
Nikita <59343780+WilfredoN@users.noreply.github.com>
2023-12-07 18:56:37 +0200

    Update AccountControllerIntegrationTest.java

--- src/test/java/bank/account/AccountControllerIntegrationTest.java
+++ src/test/java/bank/account/AccountControllerIntegrationTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("testing")
+@ActiveProfiles("test")
 class AccountControllerIntegrationTest {
 
     @Autowired
